a90a49873ab6d09fdf8aa5973f4bb49a540d86cd
João Vitor <122707909+Vitor104@users.noreply.github.com>
2025-09-20 12:37:12 -0300

    dei uma alterada no home

--- src/products/products.js
+++ src/products/products.js
@@ -5,9 +5,14 @@ import Button from 'react-bootstrap/Button';
 import { useLocation } from "react-router-dom";
 
 
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
+
+
 function Products() {
 
   const [products, setProducts] = useState(productsList);
+  const [open, setOpen] = useState(false);
   const handleRemover = (id) => {
     const updatedProducts = products.filter(product => product.id !== id);
     setProducts(updatedProducts);
@@ -17,10 +22,57 @@ function Products() {
   let nome = location.state?.user;
 
 
+
   return (
     <div className={Styles.container}>
+        <nav className={Styles.navbar}> 
+            <h1 className={Styles.navText}>ADS - PROJETO</h1>
+        </nav>
         <p className={Styles.title}>Olá, {nome}!</p>
-        <h1 className={Styles.title}>Produtos Disponíveis</h1>
+        <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+        className={Styles.button}
+      >
+        Ver produtos disponíveis
+      </Button>
+      <div style={{ minHeight: '150px' }}>
+        <Collapse in={open} dimension="width">
+          <div id="example-collapse-text">
+            <table className={`table table-striped table-bordered table-dark rounded shadow-sm ${Styles.productTable}`}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Preço (R$)</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody> 
+                {products.map(product => (
+                    <tr key={product.id}>
+                        <td className={Styles.text}>{product.id}</td>
+                        <td className={Styles.text}>{product.name}</td>
+                        <td className={Styles.text}>{product.description}</td>
+                        <td className={Styles.text}>{product.price}</td> 
+                        <td className={Styles.text}><Button className={Styles.removeBtt} onClick={() => handleRemover(product.id)} variant="info">Remover</Button></td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+          </div>
+        </Collapse>
+      </div>
+
+
+
+
+
+    {/*
+
+
         <table className={`table table-striped table-bordered table-dark rounded shadow-sm ${Styles.productTable}`}>
             <thead>
                 <tr>
@@ -43,7 +95,7 @@ function Products() {
                 ))}
             </tbody>
         </table>
-
+*/}
     </div>
   )
 }
