37487266365097a7862d6ffdcda5ddf1f5a0eb15
João Vitor <122707909+Vitor104@users.noreply.github.com>
2025-09-25 23:51:25 -0300

    Update products.js

--- src/products/products.js
+++ src/products/products.js
@@ -67,32 +67,7 @@ function Products() {
 
 
 
-    {/*
-
-
-        <table className={`table table-striped table-bordered table-dark rounded shadow-sm ${Styles.productTable}`}>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Descrição</th>
-                    <th>Preço (R$)</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody> 
-                {products.map(product => (
-                    <tr key={product.id}>
-                        <td className={Styles.text}>{product.id}</td>
-                        <td className={Styles.text}>{product.name}</td>
-                        <td className={Styles.text}>{product.description}</td>
-                        <td className={Styles.text}>{product.price}</td> 
-                        <td className={Styles.text}><Button className={Styles.removeBtt} onClick={() => handleRemover(product.id)} variant="info">Remover</Button></td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-*/}
+    
     </div>
   )
 }
